fb52100a1a7db516ad75609db737babdaa3e3a1d
Lloyd Boone <lloyd@lambapps.com>
2018-10-30 10:58:36 -0400

    Change Title Placeholders

--- src/app/layouts/App.jsx
+++ src/app/layouts/App.jsx
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Template - React Material UI</h1>
+        <h1>Template - React Semantic UI</h1>
       </div>
     );
   }
